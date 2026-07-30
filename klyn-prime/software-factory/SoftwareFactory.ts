75cfc84aa478ec4fbb6339f95c81ffc033292a78
usmanmuhd958-oss <usmanmuhd958@gmail.com>
2026-07-30 18:47:32 +0100

    feat: create KLYN Prime autonomous software factory foundation

---
+++ klyn-prime/software-factory/SoftwareFactory.ts
@@ -0,0 +1,4 @@
+nano klyn-prime/software-factory/planner/BuildPlanner.ts nano 
+klyn-prime/software-factory/architect/SystemArchitect.ts nano 
+klyn-prime/software-factory/builder/CodeBuilder.ts
+nano klyn-prime/software-factory/validator/BuildValidator.ts
