7c9ca68dd9b51ef5344d2acc17d18ac3fd550bf5
usmanmuhd958-oss <usmanmuhd958@gmail.com>
2026-08-11 13:47:02 +0100

    feat(core-runtime): add event bus infrastructure

--- packages/core-runtime/src/index.ts
+++ packages/core-runtime/src/index.ts
@@ -2,3 +2,4 @@ export * from "./HealthMonitor.js";
 export * from "./RuntimeMetrics.js";
 export * from "./DependencyChecker.js";
 export * from "./RecoveryManager.js";
+export * from "./EventBus.js";
